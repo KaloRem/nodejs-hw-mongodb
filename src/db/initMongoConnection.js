7260aba3308a545453471bdf7ca27a03444892bb
Bogdan <bogdan.paslawskii@gmail.com>
2024-12-07 15:27:29 +0100

    Steap 3 [beta]

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const initMongoConnection = async () => {
+  const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } =
+    process.env;
+
+  if (!MONGODB_USER || !MONGODB_PASSWORD || !MONGODB_URL || !MONGODB_DB) {
+    throw new Error('Missing required MongoDB environment variables');
+  }
+
+  const connectionString = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
+
+  try {
+    await mongoose.connect(connectionString);
+    console.log('Mongo connection successfully established!');
+  } catch (error) {
+    console.error('Failed to connect to MongoDB', error);
+    process.exit(1);
+  }
+};
+
+export default initMongoConnection;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import 'dotenv/config';
+import initMongoConnection from './db/initMongoConnection.js';
 import setupServer from './server.js';
 
-setupServer();
+const startApp = async () => {
+  try {
+    await initMongoConnection();
+    setupServer();
+  } catch (error) {
+    console.error('Error during app initialization', error);
+  }
+};
+
+startApp();
