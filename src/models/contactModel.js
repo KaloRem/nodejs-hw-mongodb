768563f8499f3f179cdb2b0df88b9c3089fad69f
Bogdan <bogdan.paslawskii@gmail.com>
2025-01-05 14:52:46 +0100

    v1.2

--- src/models/contactModel.js
+++ src/models/contactModel.js
@@ -1,13 +1,17 @@
-// src/models/contactModel.js
 import mongoose from 'mongoose';
 
-const contactSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  phoneNumber: { type: String, required: true },
-  email: { type: String },
-  isFavourite: { type: Boolean, default: false },
-  contactType: { type: String, required: true },
-});
+const contactSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    email: { type: String },
+    isFavourite: { type: Boolean, default: false },
+    contactType: { type: String, required: true },
+  },
+  {
+    versionKey: false,
+  },
+);
 
 const Contact = mongoose.model('Contact', contactSchema);
 
