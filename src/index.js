b883e1ef4e39a8c819e129ce4aa0f709276cc728
Bogdan <bogdan.paslawskii@gmail.com>
2025-02-23 16:24:35 +0100

    v1.0.3

--- src/index.js
+++ src/index.js
@@ -7,10 +7,13 @@ import contactsRouter from './routes/contacts.js';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 import path from 'path';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
